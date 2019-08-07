6ee1d3c7f9b411758cc4ab25e09f890eac9fd051
Elizabeth DuPre <emd222@cornell.edu>
2019-08-06 23:49:37 -0400

    Add reminder to check validator

--- content/day3/am/pybids-intro.py
+++ content/day3/am/pybids-intro.py
@@ -1,6 +1,11 @@
 from bids import BIDSLayout
 
 bids_dataset = '/home/emdupre/Desktop/ds000007'  # Change to your dataset location !
+
+# first, we want to make sure we have a valid BIDS dataset !
+# let's navigate to :
+# https://bids-standard.github.io/bids-validator/
+
 layout = BIDSLayout(bids_dataset)
 
 # Let's learn a little about the data we have
